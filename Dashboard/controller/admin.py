ec8146190975a70751a289154fa30e4204910d22
bricegnichols <bnichols@psrc.org>
2015-03-02 17:00:43 -0800

    removed duplicate folder

--- Dashboard/controller/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from controller.models import InputConfigurationValue
-
-class InputConfigurationAdmin(admin.ModelAdmin):
-	list_display = ('container','key', 'value')
-
-admin.site.register(InputConfigurationValue, InputConfigurationAdmin)
--- dashboard/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-# This will make sure the app is always imported when 
-# Django starts to that shared_task will use this app.
