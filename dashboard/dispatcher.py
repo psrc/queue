01d7e8d3045a3ac0ca639050bdcd6e670e458333
bricegnichols <bnichols@psrc.org>
2015-03-30 17:00:14 -0700

    temp code to start a log server and store results of node.py on webserver location

--- dashboard/dispatcher.py
+++ dashboard/dispatcher.py
@@ -19,6 +19,11 @@ import os
 import sys
 import Pyro4
 import numpy as np
+import pickle
+import logging, logging.handlers
+import SocketServer
+import struct
+import subprocess
 
 # List of potential model server machines on local network
 #serverlist =  ['MODELSRV' + str(i) for i in xrange(1,5)]
@@ -31,8 +36,41 @@ runid = np.random.randint(1, 10000)
 # Start the model run on the modelserver
 class StartModel():
     def start_model(self, hostname, runid):
+        
+        # Start a logging mini-server
+        subprocess.call([sys.executable, 'log_client.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+        rootLogger = logging.getLogger('')
+        hdlr = logging.FileHandler('test.log')
+        rootLogger.addHandler(hdlr)
+        rootLogger.setLevel(logging.DEBUG)
+        socketHandler = logging.handlers.SocketHandler('localhost',
+                            logging.handlers.DEFAULT_TCP_LOGGING_PORT)
+        # don't bother with a formatter, since a socket handler sends the event as
+        # an unformatted pickle
+        rootLogger.addHandler(socketHandler)
+
+        # Now, we can log to the root logger, or any other logger. First the root...
+        logging.info('Jackdaws love my big sphinx of quartz.')
+
+        # Now, define a couple of other loggers which might represent areas in your
+        # application:
+
+        logger1 = logging.getLogger('myapp.area1')
+        logger2 = logging.getLogger('myapp.area2')
+
+        logger1.debug('Quick zephyrs blow, vexing daft Jim.')
+        logger1.info('How quickly daft jumping zebras vex.')
+        logger2.warning('Jail zesty vixen who grabbed pay from quack.')
+        logger2.error('The five boxing wizards jump quickly.')
+
+        print('Connecting to node...')
         try:
+            #sys.stdout = open('file', 'w')
             proxy = Pyro4.core.Proxy("PYRONAME:" + hostname)
+            
+            SocketServer.StreamRequestHandler
+            
             proxy.runmodel(runid)
             print "Started model run on: " + hostname
 
@@ -42,6 +80,77 @@ class StartModel():
             print sys.exc_info()
             #traceback.print_exc()
             print "Couldn't connect to: " + hostname
+
+
+
+class LogRecordStreamHandler():
+    """Handler for a streaming logging request.
+
+    This basically logs the record using whatever logging policy is
+    configured locally.
+    """
+
+    def handle(self):
+        """
+        Handle multiple requests - each expected to be a 4-byte length,
+        followed by the LogRecord in pickle format. Logs the record
+        according to whatever policy is configured locally.
+        """
+        while True:
+            chunk = self.connection.recv(4)
+            if len(chunk) < 4:
+                break
+            slen = struct.unpack('>L', chunk)[0]
+            chunk = self.connection.recv(slen)
+            while len(chunk) < slen:
+                chunk = chunk + self.connection.recv(slen - len(chunk))
+            obj = self.unPickle(chunk)
+            record = logging.makeLogRecord(obj)
+            self.handleLogRecord(record)
+
+    def unPickle(self, data):
+        return pickle.loads(data)
+
+    def handleLogRecord(self, record):
+        # if a name is specified, we use the named logger rather than the one
+        # implied by the record.
+        if self.server.logname is not None:
+            name = self.server.logname
+        else:
+            name = record.name
+        logger = logging.getLogger(name)
+        # N.B. EVERY record gets logged. This is because Logger.handle
+        # is normally called AFTER logger-level filtering. If you want
+        # to do filtering, do it at the client end to save wasting
+        # cycles and network bandwidth!
+        logger.handle(record)
+
+class LogRecordSocketReceiver(SocketServer.ThreadingTCPServer):
+    """simple TCP socket-based logging receiver suitable for testing.
+    """
+
+    allow_reuse_address = 1
+
+    def __init__(self, host='localhost',
+                 port=logging.handlers.DEFAULT_TCP_LOGGING_PORT,
+                 handler=LogRecordStreamHandler):
+        SocketServer.ThreadingTCPServer.__init__(self, (host, port), handler)
+        self.abort = 0
+        self.timeout = 1
+        self.logname = None
+
+    def serve_until_stopped(self):
+        import select
+        abort = 0
+        while not abort:
+            rd, wr, ex = select.select([self.socket.fileno()],
+                                       [], [],
+                                       self.timeout)
+            if rd:
+                self.handle_request()
+            abort = self.abort
+
+
 
 
 # Try connecting to a server
