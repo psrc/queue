92a7e3d85fa4edec29bb2b7913cb1fbd06b68526
bricegnichols <bnichols@psrc.org>
2015-04-01 12:02:43 -0700

    fixed decorator function

--- remote.py
+++ remote.py
@@ -1,7 +1,9 @@
+from __future__ import absolute_import
+
 from celery import Celery
 
 app = Celery('tasks', backend='amqp',
-                      broker='amqp://psrc:psrc1@10.10.11.89/psrcvhost')
-
+                      broker='amqp://psrc:psrc1@10.10.11.194/psrcvhost')
+@app.task
 def add(x, y):
      return x + y
