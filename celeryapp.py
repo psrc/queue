c20146760cf7af3a2ca5e0eb07caaac8451daed4
bricegnichols <bnichols@psrc.org>
2015-04-01 09:35:11 -0700

    added logging

--- celeryapp.py
+++ celeryapp.py
@@ -2,15 +2,23 @@ from __future__ import absolute_import
 
 from celery import Celery
 
-app = Celery(
-             broker='amqp://',
-             backend='amqp://',
-             include=['tasks'])
+# app = Celery(
+#              broker='amqp://',
+#              backend='amqp://',
+#              include=['tasks'])
+
+# instantiate Celery object
+celery = Celery(include=[
+                         'tasks'
+                        ])
 
 # Optional configuration, see the application user guide.
-app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
-)
+# app.conf.update(
+#     CELERY_TASK_RESULT_EXPIRES=3600,
+# )
+
+# import celery config file
+celery.config_from_object('celeryconfig')
 
 if __name__ == '__main__':
     app.start()
