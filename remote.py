4f432fd6786ca5c11b57051246117dc12d32a887
bricegnichols <bnichols@psrc.org>
2015-04-08 12:03:04 -0700

    removed celery testing files, never should have been version controlled

--- remote.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from celery import Celery
-
-app = Celery('tasks', backend='amqp',
-                      broker='amqp://psrc:psrc1@10.10.11.194/psrcvhost')
-@app.task
-def add(x, y):
-     return x + y
