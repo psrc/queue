5f83e15bd9ec3a4cd0a6a293a9f2462fb026d225
Billy Charlton <bcharlton@psrc.org>
2015-04-16 13:15:12 -0700

    get /index working.  (still pointing to launch page though)

--- controller/urls.py
+++ controller/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import patterns, url
 from controller import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'register/$', views.register, name='register'),
-	url(r'login/$', views.user_login, name='login'),
-	url(r'logout/$', views.user_logout, name='logout'),
-	url(r'about/$', views.about, name='about'),
-	url(r'soundcast/$', views.soundcast, name='soundcast'),
+    url(r'^$', views.index, name='index'),
+    url(r'register/$', views.register, name='register'),
+    url(r'login/$', views.user_login, name='login'),
+    url(r'logout/$', views.user_logout, name='logout'),
+    url(r'about/$', views.about, name='about'),
+    url(r'soundcast/$', views.soundcast, name='soundcast'),
     url(r'run-sc/$', views.run_soundcast, name='run_soundcast'),
     url(r'4k/$', views.fourkay, name='fourkay'),
     url(r'monitor/$', views.monitor, name='monitor'),
-	)
+    )
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dashboard.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('controller.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^controller/', include('controller.urls')),
 )
