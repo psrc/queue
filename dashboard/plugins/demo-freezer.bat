dc0c12478615e5ee0d9bea75c9df8d55bed1d593
Billy Charlton <billy@worldofbilly.com>
2015-11-15 17:29:55 -0800

    switch from a git archive to borg backup -- better compression but totally stupid
    This may be a huge paint point: git and git fat are not great, and
    git-fat in particular doesn't do deduplication of similar or even identical
    files.
    
    Borg Backup does -- but it's super wonky and not well-supported on windows.
    We'll have to see how it does. Might switch back and also open to LOTS
    of other alternatives, if they exist.
    
    git/git-fat have advantage that it's a one-liner to get a .zip file.
    borg-backup it's easy to restore the *folder*, and deduplication is rad.

--- dashboard/plugins/demo-freezer.bat
+++ dashboard/plugins/demo-freezer.bat
@@ -1,31 +1,14 @@
 @echo off
 :: FREEZER.BAT - save a copy of all model inputs before it is run
+:: Requires %TOOL% and %LOCATION% to be set
+:: LOCATION needs to be in Cygwin format: /cygdrive/c/archive/location
 set TOOL=Demo
+set LOCATION=/cygdrive/x/DSA/Queue-Archive
 
-:: ARCHIVE is the path to the inputs git repository
-set ARCHIVE_DRIVE=X:
-set ARCHIVE=X:\DSA\Queue-Archive\inputs\%TOOL%
+:: Use borg-backup to copy inputs to the shared borg folder.
+set ARCHIVE=%LOCATION%::%TOOL%
+c:\cygwin64\bin\bash.exe -i -c "/usr/bin/borg create --exclude '.git*' --exclude '.idea' --exclude '*.pyc' $ARCHIVE ."
 
-:: Use rsync to add/remove all files except Git configuration
-:: (this cmd syncs files from current working directory to X: folder above)
-:: (rsync server settings are on dsadashboard in /etc/rsyncd.conf)
-::rsync -vrltz --delete --exclude '.git*' * /cygdrive/c/Users/Billy/Desktop/Archive/inputs/%TOOL%
-rsync -vrltz --delete --exclude '.git*' * rsync://dsadashboard/tools/%TOOL%
-if ERRORLEVEL 1 exit 6
 
-:: switch to network archive drive and folder, and commit changes
-%ARCHIVE_DRIVE%
-cd %ARCHIVE%
 
-:: -A option does a full add/remove of all files, whether new, existing, or deleted.
-git add -A .
 
-:: Record changes as if user did it themselves
-git commit -m "$PROJECT / Series $SERIES / Version $TAG" --author "Automated <no-one@psrc.org>"
-
-:: Git-fat stores large files separately so repo doesn't get huge
-:: See https://github.com/cyaninc/git-fat
-git fat push
-
-:: Tag with the run log number
-if defined RUNLOG git tag %RUNLOG%
--- dashboard/plugins/soundcast-freezer.bat
+++ dashboard/plugins/soundcast-freezer.bat
@@ -1,30 +1,10 @@
 @echo off
 :: FREEZER.BAT - save a copy of all model inputs before it is run
+:: Requires %TOOL% and %LOCATION% to be set
+:: LOCATION needs to be in Cygwin format: /cygdrive/c/archive/location
 set TOOL=SoundCast
+set LOCATION=/cygdrive/x/DSA/Queue-Archive
 
-:: ARCHIVE is the path to the inputs git repository
-set ARCHIVE_DIR=X:
-set ARCHIVE=X:\DSA\Dashboard-Freezer\inputs\%TOOL%
-
-:: Use rsync to add/remove all files except Git configuration
-:: (this cmd syncs files from current working directory to X: folder above)
-:: (rsync server settings are on dsadashboard in /etc/rsyncd.conf)
-rsync -vrltz --delete --exclude '.git*' * rsync://dsadashboard/tools/%TOOL%
-if ERRORLEVEL 1 exit 6
-
-:: switch to network archive drive and folder, and commit changes
-%ARCHIVE_DIR%
-cd %ARCHIVE%
-
-:: -A option does a full add/remove of all files, whether new, existing, or deleted.
-git add -A .
-
-:: Record changes as if user did it themselves
-git commit -m "$PROJECT / Series $SERIES / Version $TAG" --author "Automated <noone@psrc.org>"
-
-:: Git-fat stores large files separately so repo doesn't get huge
-:: See https://github.com/cyaninc/git-fat
-git fat push
-
-:: Tag with the run log number
-if defined RUNLOG git tag %RUNLOG%
+:: Use borg-backup to copy inputs to the shared borg folder.
+set ARCHIVE=%LOCATION%::%TOOL%
+c:\cygwin64\bin\bash.exe -i -c "/usr/bin/borg create --exclude '.git*' --exclude '.idea' --exclude '*.pyc' $ARCHIVE ."
