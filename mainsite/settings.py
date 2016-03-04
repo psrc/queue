3887e5cc929fd1b623cd1787a2b3d1136cc2a5df
Billy Charlton <billy@worldofbilly.com>
2016-03-04 11:20:38 -0800

    add missing template 'context processor'. i hate django 1.9

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
+                'django.core.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
