9f4186c97e4b0d92817b7451f95bbb99b125857c
Billy Charlton <billy@worldofbilly.com>
2016-06-09 12:30:06 -0700

    set up flask-migrate, but don't activate it yet

--- run-server.py
+++ run-server.py
@@ -1,6 +1,24 @@
+import os
 import plugins
-from server import app
+from server import app, manager
+
+EXTRA_WATCH_DIRS = ['server/templates/']
 
 print '\nPSRC QUEUE'
 plugins.register_plugins(app)
-app.run()
+
+# Flask doesn't watch template files in debug mode
+extra_files = EXTRA_WATCH_DIRS[:]
+for extra_dir in EXTRA_WATCH_DIRS:
+    print 'Watching extra dir for changes:', extra_dir
+    for dirname, dirs, files in os.walk(extra_dir):
+        for filename in files:
+            filename = os.path.join(dirname, filename)
+            if os.path.isfile(filename):
+                extra_files.append(filename)
+
+print extra_files
+
+# manager.run()
+app.run(debug=True, extra_files=extra_files)
+
--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 Bootstrap(app)
+
+# DB and migration support
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # imports are below app instantiation to avoid circular references
 from server import views, models, forms
